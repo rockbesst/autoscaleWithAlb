889fa2f7984ff155cb20f9057a6812449c0561da
Rockbesst Win <rockbesst@gmail.com>
2021-07-06 12:05:56 +0300

    fix userdata

--- ready_webserver.sh
+++ ready_webserver.sh
@@ -2,6 +2,6 @@
 yum -y update
 amazon-linux-extras install -y nginx1
 ip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
-echo "<h2>Server IP is $ip.</h2>" > /var/www/html/index.html
+echo "<h2>Server IP is $ip.</h2>" > /usr/share/nginx/html/index.html
 sudo service nginx start
 chkconfig nginx on
