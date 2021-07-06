21e9532c5984fdcea85f7c0782951f6d72353a7d
Rockbesst Win <rockbesst@gmail.com>
2021-07-06 12:27:17 +0300

    added autoscaling attachments

--- autoscale.tf
+++ autoscale.tf
@@ -1,19 +1,32 @@
 resource "aws_launch_configuration" "as_conf" {
-  name          = "web_config"
-  image_id      = data.aws_ami.amazon_linux.id
-  instance_type = var.instance_type
+    name          = "web_config"
+    image_id      = data.aws_ami.amazon_linux.id
+    instance_type = var.instance_type
+    user_data = file("ready_webserver.sh")
+    key_name = var.ssh_key
+    tags = merge(var.tags, {Name = "as_test"})
 }
 
 resource "aws_autoscaling_group" "as" {
   name                      = "autoscale"
-  max_size                  = 5
-  min_size                  = 2
+  max_size                  = 4
+  min_size                  = 1
   health_check_grace_period = 60
-  health_check_type         = "ELB"
+  health_check_type         = "EC2"
   desired_capacity          = 2
   force_delete              = true
   launch_configuration      = aws_launch_configuration.as_conf.name
   vpc_zone_identifier       = [data.aws_subnet.sub1.id, data.aws_subnet.sub2.id]
+}
+
+resource "aws_autoscaling_attachment" "asg_attachment_alb" {
+  autoscaling_group_name = aws_autoscaling_group.as.id
+  elb                    = aws_elb.MainLoadBalancer.id
+}
+
+resource "aws_autoscaling_attachment" "asg_attachment_tg" {
+  autoscaling_group_name = aws_autoscaling_group.as.id
+  alb_target_group_arn   = aws_lb_target_group.tg_main.arn
 }
 
 resource "aws_autoscaling_policy" "as_up" {
