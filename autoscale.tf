79cfb425355520fa5b978c655bf98af4d4f91f43
Rockbesst Win <rockbesst@gmail.com>
2021-07-06 11:59:59 +0300

    added metric_alarms

--- autoscale.tf
+++ autoscale.tf
@@ -15,3 +15,45 @@ resource "aws_autoscaling_group" "as" {
   launch_configuration      = aws_launch_configuration.as_conf.name
   vpc_zone_identifier       = [data.aws_subnet.sub1.id, data.aws_subnet.sub2.id]
 }
+
+resource "aws_autoscaling_policy" "as_up" {
+    name = "agents-scale-up"
+    scaling_adjustment = 1
+    adjustment_type = "ChangeInCapacity"
+    cooldown = 300
+    autoscaling_group_name = aws_autoscaling_group.as.name
+}
+
+resource "aws_autoscaling_policy" "as_down" {
+    name = "agents-scale-down"
+    scaling_adjustment = -1
+    adjustment_type = "ChangeInCapacity"
+    cooldown = 300
+    autoscaling_group_name = aws_autoscaling_group.as.name
+}
+
+resource "aws_cloudwatch_metric_alarm" "trigger_as_up" {
+  alarm_name          = "trigger_as_up"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = "70"
+  alarm_description = "This metric monitors ec2 cpu utilization and trigger upscaling"
+  alarm_actions     = [aws_autoscaling_policy.as_up.arn]
+}
+
+resource "aws_cloudwatch_metric_alarm" "trigger_as_down" {
+  alarm_name          = "trigger_as_down"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = "70"
+  alarm_description = "This metric monitors ec2 cpu utilization and trigger downscaling"
+  alarm_actions     = [aws_autoscaling_policy.as_down.arn]
+}
