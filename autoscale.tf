8c4b5ab4427a059487c53c48a5df1ba8bac3ffba
Rockbesst Win <rockbesst@gmail.com>
2021-07-08 20:15:13 +0300

    fix

--- autoscale.tf
+++ autoscale.tf
@@ -24,7 +24,6 @@ resource "aws_autoscaling_policy" "as_policy" {
     policy_type = "TargetTrackingScaling"
     scaling_adjustment = 1
     adjustment_type = "ChangeInCapacity"
-    cooldown = 300
     autoscaling_group_name = aws_autoscaling_group.as.name
     target_tracking_configuration {
         predefined_metric_specification {
